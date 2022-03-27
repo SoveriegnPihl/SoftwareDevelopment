5115237399af3e8fa838669885f149b240d4f7fe
Marcus <marcuspihl01@gmail.com>
2022-03-27 13:02:03 +0200

    Added classes, with some initial variables for a nice start

---
+++ src/src/java/dtu/softwarehus/SoftwareHuset.java
@@ -0,0 +1,22 @@
+package dtu.softwarehus;
+
+import dtu.employees.Developer;
+import dtu.project.Project;
+import dtu.project.Report;
+
+import java.util.ArrayList;
+
+public class SoftwareHuset {
+    ArrayList<Report> reports;
+    ArrayList<Developer> developers;
+    ArrayList<Project> projects;
+
+
+    void createProject(){
+
+    }
+
+    void addDeveloper(){
+
+    }
+}
