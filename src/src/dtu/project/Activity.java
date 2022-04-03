37cb4c1e55b3d681abe89a8f8e9d83aa0a0e3f0d
victoreydeliberoth <eyde.liberoth@gmail.com>
2022-04-03 10:40:45 +0200

    createActivity usecase

--- src/src/dtu/project/Activity.java
+++ src/src/dtu/project/Activity.java
@@ -8,7 +8,20 @@ public class Activity {
     int estimatedTime;
     ArrayList<Developer> developers;
     String name;
+    int projectID;
+
+    public Activity (String projectName,String name, int estimatedTime) {
+        this.estimatedTime = estimatedTime;
+        this.name = name;
+        this.projectName = projectName;
+        projectId = (project.nextId++) + 22000;
 
 
+    }
+
+
+    public void printActivity(){
+        System.out.println("Project name: " + projectName + " project id: " + projectId + " activity name: " + name + " estimated time: " + estimatedTime);
+    }
 
 }
