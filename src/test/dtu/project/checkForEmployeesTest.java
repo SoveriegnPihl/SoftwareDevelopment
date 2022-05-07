7a219dcbaedfa24b41989dbf07f8eed88bc4a622
SovereignPihl <marcuspihl01@gmail.com>
2022-05-07 13:58:43 +0200

    Added tests and some stuff

---
+++ src/test/dtu/project/checkForEmployeesTest.java
@@ -0,0 +1,39 @@
+package dtu.employees;
+import dtu.softwarehus.SoftwareHuset;
+import dtu.project.Project;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import static org.junit.Assert.assertTrue;
+
+public class checkForEmployeesTest {
+    int id = 22001;
+    SoftwareHuset sf = new SoftwareHuset();
+
+    @And("There is a project manager")
+    public void there_is_project_manager(){
+        Project p = sf.getProject(String.valueOf(id));
+        assertTrue("Project has project manager", !(p.getManager().initials.equals("NULL")));
+    }
+    @And ("There is a list of employees")
+    public void there_is_list_of_employees(){
+        assertTrue(sf.listDevs().size() > 0);
+    }
+
+
+    @Then ("A list of available employees is made")
+    public void a_list_of_available_employees(){
+       assertTrue(!(sf.listAvailableDevelopers().isEmpty()));
+    }
+
+    @And ("There is a list with available employees")
+    public void there_is_list_of_available_employees(){
+        assertTrue(sf.availableDevelopers.size() > 0);
+    }
+
+
+
+
+
+}
